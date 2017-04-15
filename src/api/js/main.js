6f7127fb148f23ba7d82acb35f64579282c1e150
Björn Bosse <b.bosse1991@gmail.com>
2017-04-15 21:39:33 +0200

    Show message to user until the selected action is completed

--- src/api/js/main.js
+++ src/api/js/main.js
@@ -2,30 +2,34 @@ var rolloPositions = [0.33, 0.5, 0.66];
 
 $(document).on("pagecreate", function() {
     $('#tabOne').click();
-
-    $.notifyBar({ cssClass: 'success', html: 'Rollo wird geöffnet!' });
 });
 
 function onUpClick() {
+    $.notifyBar({ cssClass: 'warning', html: 'Vorgang läuft...', delay: 999999 });
+
     $.get('rollo.php?up=1', function(data) {
         $.notifyBar({ cssClass: 'success', html: 'Rollo wird geöffnet!' });
     });
 }
 
 function onStopClick() {
+    $.notifyBar({ cssClass: 'warning', html: 'Vorgang läuft...', delay: 999999 });
+
     $.get('rollo.php?stop=1', function(data) {
         $.notifyBar({ cssClass: 'success', html: 'Rollo wurde gestoppt!' });
     });
 }
 
 function onDownClick() {
+    $.notifyBar({ cssClass: 'warning', html: 'Vorgang läuft...', delay: 999999 });
+
     $.get('rollo.php?down=1', function(data) {
         $.notifyBar({ cssClass: 'success', html: 'Rollo wird geschlossen!' });
     });
 }
 
 function onShutClick(index) {
-    $.notifyBar({ cssClass: 'warning', html: 'Vorgang läuft...' });
+    $.notifyBar({ cssClass: 'warning', html: 'Vorgang läuft...', delay: 999999 });
 
     var rolloPos = rolloPositions[index];
     $.get('rollo.php?position=' + rolloPos, function(data) {
