112e159186d72ff1769174512b323cbf4cb80e4d
Björn Bosse <b.bosse1991@gmail.com>
2017-04-15 21:31:08 +0200

    Use better notification plugin

--- src/api/js/main.js
+++ src/api/js/main.js
@@ -1,35 +1,34 @@
 var rolloPositions = [0.33, 0.5, 0.66];
 
 $(document).on("pagecreate", function() {
-    $.notify.defaults({ position: 'top left' });
-    //$("#tabs").tabs("option", "active", 0);
-
     $('#tabOne').click();
+
+    $.notifyBar({ cssClass: 'success', html: 'Rollo wird geöffnet!' });
 });
 
 function onUpClick() {
     $.get('rollo.php?up=1', function(data) {
-        $.notify('Rollo wird geöffnet!', 'success');
+        $.notifyBar({ cssClass: 'success', html: 'Rollo wird geöffnet!' });
     });
 }
 
 function onStopClick() {
     $.get('rollo.php?stop=1', function(data) {
-        $.notify('Rollo wurde gestoppt!', 'success');
+        $.notifyBar({ cssClass: 'success', html: 'Rollo wurde gestoppt!' });
     });
 }
 
 function onDownClick() {
     $.get('rollo.php?down=1', function(data) {
-        $.notify('Rollo wird geschlossen!', 'success');
+        $.notifyBar({ cssClass: 'success', html: 'Rollo wird geschlossen!' });
     });
 }
 
 function onShutClick(index) {
-    $.notify('Vorgang läuft...', { autoHide: false, className: 'info' });
+    $.notifyBar({ cssClass: 'warning', html: 'Vorgang läuft...' });
 
     var rolloPos = rolloPositions[index];
     $.get('rollo.php?position=' + rolloPos, function(data) {
-        $.notify('Rollo ist in Position!', 'success');
+        $.notifyBar({ cssClass: 'success', html: 'Rollo ist in Position!' });
     });
 }
